2a986b76234168e4ee0707ec751327fd0706f536
MoonlapseVertigo <150127564+xXxandersandxXx@users.noreply.github.com>
2024-04-23 09:47:36 +0200

    created money

---
+++ Assets/scripts/money.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class money : MonoBehaviour
+{
+    public int coins = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+  
+}
+    
+    
+
+
+
+
+
