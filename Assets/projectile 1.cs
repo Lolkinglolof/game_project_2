8e0faf0850e442fa41875337a583ab114246ece8
Lolkinglolof <Torben.Thiemer@gmail.com>
2024-04-30 10:16:55 +0200

    fix
    shoot tower

---
+++ Assets/projectile 1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile1 : MonoBehaviour
+{
+    public float speed = 7.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(speed*Time.deltaTime,0);
+
+    }
+}
--- Assets/small shooting tower.cs
+++ Assets/small shooting tower.cs
@@ -2,37 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class smallshootingtower : MonoBehaviour
+public class cointower : MonoBehaviour
 {
-    public GameObject bullet;
-    public float shoottimer;
-    public float shoottime;
-    public float damagetime;
-    public float damagetimer;
-    public float health;
+    public float bullettime = 2.4f;
+    private float ShootTimer;
+    public GameObject projectile;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        shoottimer = Time.deltaTime + shoottimer;
-        if (shoottimer >= shoottime)
+        if (ShootTimer >= 3)
         {
-            Instantiate(bullet);
-            shoottimer = 0;
+            ShootTimer = 0;
+            GameObject ah = Instantiate(projectile, new Vector3(gameObject.transform.position.x + 0.5f, gameObject.transform.position.y, -0.6f), Quaternion.identity);
+            Destroy(ah,bullettime);
+        }
+        else
+        {
+            ShootTimer += Time.deltaTime;
+            Debug.Log(ShootTimer);
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        damagetimer = Time.deltaTime;
-        if (damagetimer >= damagetime)
-        {
-            health = health - 1;
-            damagetimer = 0;
-        }
-    }
+
 }
